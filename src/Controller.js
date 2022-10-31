8189df73dffd0c8e6637e731241063ae3edc9f8e
RollingHog <46373241+RollingHog@users.noreply.github.com>
2022-10-31 07:47:54 +0300

    proper /route mapper

--- src/Controller.js
+++ src/Controller.js
@@ -18,6 +18,8 @@ class Controller {
     this.routes = {}
     this.routes[HTTP_METHOD.GET] = {}
     this.routes[HTTP_METHOD.POST] = {}
+
+    this.lastDoc = null
   }
 
   /**
@@ -29,7 +31,12 @@ class Controller {
     method = method.toUpperCase()
     if(this.routes[method] == undefined) throw new Error(`Method ${method} not allowed`)
 
-    this.routes[method][route] = callback
+    this.routes[method][route] = { callback }
+
+    if(this.lastDoc !== null) {
+      this.routes[method][route].doc = this.lastDoc
+      this.lastDoc = null
+    }
 
   }
 
@@ -38,7 +45,7 @@ class Controller {
    * @param {ControllerCallbackFunction} callback
    */
   GET(route, callback) {
-    this.add(HTTP_METHOD.GET, route, callback)
+    return this.add(HTTP_METHOD.GET, route, callback)
   }
 
   /**
@@ -46,7 +53,12 @@ class Controller {
    * @param {ControllerCallbackFunction} callback
    */
   POST(route, callback) {
-    this.add(HTTP_METHOD.POST, route, callback)
+    return this.add(HTTP_METHOD.POST, route, callback)
+  }
+
+  doc(str) {
+    this.lastDoc = str
+    return this
   }
 
   /**
@@ -58,7 +70,7 @@ class Controller {
     const {method, url} = req
     let result = null
     if(this.routes[method][url])
-      result = this.routes[method][url](req, res, body)
+      result = this.routes[method][url].callback(req, res, body)
     else {
       throw new CustomError(404, 'route do not exist')
     }
@@ -72,7 +84,21 @@ class Controller {
 
 const controller = new Controller()
 
-controller.GET('/routes', () => JSON.stringify(this.routes,  (key, val) => typeof val === 'function' ? '[Function]' : val))
-controller.GET('/version', () => VERSION)
+controller
+.doc('maps all existing routes; should be DEV mode only')
+.GET('/routes', () => {
+  const list = []
+  for(let method in controller.routes) {
+    for(let route in controller.routes[method]) {
+      // list.push({method: i, url: j})
+      list.push(`<tr><td>${method}</td><td><a href="${route}">${route}</a></td><td>${controller.routes[method][route].doc}</td></tr>`)
+    }
+  }
+  return `<table>${list.join('')}</table>`
+})
+
+controller
+.doc('returns server version')
+.GET('/version', () => VERSION)
 
 export default controller

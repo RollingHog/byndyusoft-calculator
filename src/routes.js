923bea8c86d5d5f46d69a751255800d1086b175a
RollingHog <46373241+RollingHog@users.noreply.github.com>
2022-10-31 05:25:08 +0300

    Create routes.js

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { controller } from "./Controller.js"
+import CustomError from "./misc/CustomError.js"
+
+controller.GET('/test', function(req, res) {
+  throw new CustomError(666, 'test')
+})
+
+export default controller

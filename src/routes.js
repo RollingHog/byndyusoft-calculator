18a81e6db34b8ed282e481ebf75969e7c4cdec84
RollingHog <46373241+RollingHog@users.noreply.github.com>
2022-11-21 02:08:05 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,13 @@ import CustomError from "./misc/CustomError.js"
 
 controller
 .doc('throws error with custom code')
-.GET('/test_ce', function () {
+.GET('/test/ce', function () {
   throw new CustomError(666, 'test')
 })
 
 controller
 .doc('throws regular js error')
-.GET('/test_e', function () {
+.GET('/test/e', function () {
   throw new Error('default error')
 })
 
